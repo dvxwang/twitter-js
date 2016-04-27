891bf1060f6322fdd40196b208d826e57603c6a1
dvxwang <dvxwang@gmail.com>
2016-04-27 10:10:59 -0400

    (DW)Latest twitter updates

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var express = require( 'express' );
+var swig = require( 'swig' );
 var app = express(); // creates an instance of an express application
+var routes = require('./routes');
+var socketio = require('socket.io');
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+var server = app.listen(3000, function() {
+  console.log("Port up and running!");
+})
+var io = socketio.listen(server);
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
+
+app.use(express.static('public'));
 
 app.use(function(req,res,next) {
   console.log(req.method+" "+req.url);
@@ -7,6 +25,15 @@ app.use(function(req,res,next) {
   console.log("Eventual status code");
 })
 
-var server = app.listen(3000, function() {
-  console.log("Port up and running!");
-})
+app.use('/', routes(io));
+
+io.on('new_tweet', function(call){
+  // Create an element for each piece of data in the phone call object.
+  console.log("Reached IO process: ");
+  console.log("Reached objetc: ",call);
+  var newTweet = document.createElement('li');
+  newTweet.textContent = "David"+call.name+call.text;
+ 
+  // Append the new object to the #phone-calls div.
+  document.getElementById('TweetLog').appendChild(newTweet);
+});
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+module.exports = function(io) {
+
+  var express = require('express');
+  var router = express.Router();
+  // could use one line instead: var router = require('express').Router();
+  var tweetBank = require('../tweetBank');
+  var bodyParser = require('body-parser');
+
+  router.use(bodyParser.urlencoded({ extended: false }))
+  router.use(bodyParser.json())
+
+  router.post('/tweets/', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name,text);
+    io.emit('new_tweet', {name: name, text:text});
+    res.redirect("/");
+  });
+
+  router.get('/', function (req, res) {
+    var tweets = tweetBank.list();
+    res.render( 'index', { title: 'Twitter.js', tweets: tweets} );
+  });
+
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var tweets = tweetBank.find( {name: name} );
+    console.log("tweeter: ",name);
+    res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: tweets, username: name, showForm: true} );
+  });
+
+  router.get('/tweets/:id', function(req, res) {
+    var idNum = +req.params.id;
+    var tweets = tweetBank.find( {"counter": idNum} );
+    res.render( 'index', { title: 'Twitter.js', tweets: tweets} );
+  });
+
+  return router;
+
+};
